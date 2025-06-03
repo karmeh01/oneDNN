f99ebd67859a6ae181df6850e81fa42febb017a4
Kareem Mehanna <kareemhisham.mehanna@arm.com>
2025-06-03 09:06:15 +0000

    Updated copyright information

--- src/cpu/aarch64/jit_brdgmm_dw_conv.cpp
+++ src/cpu/aarch64/jit_brdgmm_dw_conv.cpp
@@ -1,6 +1,7 @@
 /*******************************************************************************
 * Copyright 2021-2023 Intel Corporation
 * Copyright 2024 FUJITSU LIMITED
+* Copyright 2025 Arm Ltd. and affiliates
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
