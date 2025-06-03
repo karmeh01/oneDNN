8889216cc94122f02e199242d80bf8a55b0fe720
Kareem Mehanna <kareemhisham.mehanna@arm.com>
2025-06-03 10:05:49 +0000

    Clang format

--- src/cpu/aarch64/jit_brdgmm_dw_conv.cpp
+++ src/cpu/aarch64/jit_brdgmm_dw_conv.cpp
@@ -383,7 +383,7 @@ status_t brdgmm_dw_convolution_fwd_t<isa>::pd_t::init_brdgmm_conf() {
                                   : (jcp.ch_block == 8 ? format_tag::dhwioG8g
                                                        : format_tag::dhwioG4g))
             : (jcp.ch_block == 16 ? format_tag::hwioG16g
-                                 : (jcp.ch_block == 8 ? format_tag::hwioG8g
+                                  : (jcp.ch_block == 8 ? format_tag::hwioG8g
                                                        : format_tag::hwioG4g));
 
     const memory_desc_wrapper weights_d(&weights_md_);
